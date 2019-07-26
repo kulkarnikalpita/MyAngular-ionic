e556af72ab889719ca42eccfcc9676ed3b5bca67
kulkarnikalpita <52975983+kulkarnikalpita@users.noreply.github.com>
2019-07-26 20:00:20 +0530

    commit

--- auth.page.ts
+++ auth.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -8,14 +10,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-
-  constructor(private authService: AuthService, private router: Router) { }
+   isLoading = false;
+   isLogin = true;
+  constructor(private authService: AuthService, private router: Router,private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
   }
 
   onLogin() {
+     this.isLoading = true;
      this.authService.login();
-     this.router.navigateByUrl('/places/tabs/discover');
+     this.loadingCtrl.create({ keyboardClose: true, message: 'Logging in..'})
+     .then(loadingEl => {
+       loadingEl.present();
+       setTimeout(() => {
+       this.isLoading = false;
+       loadingEl.dismiss();
+       this.router.navigateByUrl('/places/tabs/discover');
+     }, 1500);
+    });
   }
-}
+  onSwitchAuthMode() {
+this.isLogin = !this.isLogin;
+  }
+ onSubmit(form: NgForm) {
+  if (!form.valid) {
+    return;
+  }
+  const email = form.value.email;
+  const password = form.value.password;
+  console.log(email, password);
+
+  if (this.isLogin) {
+  // send a request to login servers
+  } else {
+    // send a request to signup servcers
+  }
+  }
+ }
+
